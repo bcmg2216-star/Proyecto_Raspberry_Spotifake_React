75996e2386ef8597047956f2d00520f0a31d2bec
bcmg2216-star <beacobo22@gmail.com>
2026-03-01 22:47:04 +0100

    Areglando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import logo from './assets/logo.png'
+import logo from '../public/logo.png'
 
 function App() {
     const [user, setUser] = useState(null) // guardamos los datos y el token
